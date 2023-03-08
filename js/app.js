fb0e9df63a56456195350953847d191cf9bffcb2
Heather Holcomb <holcombheather@gmail.com>
2023-03-07 22:31:31 -0800

    Updated files to have the basics of js working for User Store 1 Feature Task 3

--- js/app.js
+++ js/app.js
@@ -1,2 +1,51 @@
-'use strict'
+'use strict';
 
+let questionOne = prompt('Question One type yes or no').toLowerCase();
+
+if (questionOne === 'yes') {
+  console.log('Correct');
+  alert(questionOne + ' is correct!');
+} else {
+  console.log('Wrong');
+  alert('Sorry that is not correct');
+}
+
+let questionTwo = prompt('Question Two type yes or no').toLowerCase();
+
+if (questionTwo === 'no') {
+  console.log('Correct');
+  alert(questionTwo + ' is correct!');
+} else {
+  console.log('Wrong');
+  alert('Sorry that is not correct');
+}
+
+let questionThree = prompt('Question Three type yes or no').toLowerCase();
+
+if (questionThree === 'no') {
+  console.log('Correct');
+  alert(questionThree + ' is correct!');
+} else {
+  console.log('Wrong');
+  alert('Sorry that is not correct');
+}
+
+let questionFour = prompt('Question Four type yes or no').toLowerCase();
+
+if (questionFour === 'no') {
+  console.log('Correct');
+  alert(questionFour + ' is correct!');
+} else {
+  console.log('Wrong');
+  alert('Sorry that is not correct');
+}
+
+let questionFive = prompt('Question Five type yes or no').toLowerCase();
+
+if (questionFive === 'no') {
+  console.log('Correct');
+  alert(questionFive + ' is correct!');
+} else {
+  console.log('Wrong');
+  alert('Sorry that is not correct');
+}
